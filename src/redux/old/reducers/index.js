e869e8767cb2f91eb9a357c88dd123e22b7fb475
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-10 21:21:07 +0100

    move old redux actions & reducers

---
+++ src/redux/old/reducers/index.js
@@ -0,0 +1,61 @@
+import { combineReducers } from "redux";
+import { getId } from "../../utils";
+
+const activeFilter = (state = "SHOW_ALL", action) => {
+  switch (action.type) {
+    case "SET_ACTIVE_FILTER":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  const { payload } = action;
+
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: getId(state),
+          content: payload,
+          completed: false,
+          important: false,
+        },
+      ];
+    case "DELETE_TODO":
+      return state.filter(todo => todo.id !== payload);
+    case "DELETE_ALL":
+      return [];
+    case "TOGGLE_IMPORTANT":
+      return state.map(todo => {
+        if (todo.id === payload) {
+          return {
+            ...todo,
+            important: !todo.important,
+          };
+        }
+        return todo;
+      });
+    case "TOGGLE_COMPLETE":
+      return state.map(todo => {
+        if (todo.id === payload) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    case "SET_TODOS":
+      return payload;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  activeFilter,
+  todos,
+});
